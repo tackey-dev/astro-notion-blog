6a417bba47ebedcd4317ec30c352302da0dd8760
tackey-dev <k.takiya.dev@gmail.com>
2026-01-13 00:26:33 +0900

    npm install @astrojs/sitemap
    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
 
+import sitemap from '@astrojs/sitemap';
+
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
@@ -42,5 +44,6 @@ export default defineConfig({
     CustomIconDownloader(),
     FeaturedImageDownloader(),
     PublicNotionCopier(),
+    sitemap()
   ],
 });
